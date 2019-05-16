626c8e8884069c68a18b040bc7041b4960841c3a
Richard Sanchez <superpoptart1@gmail.com>
2019-05-15 18:02:55 -0600

    Trying to put site on github (bootstrap)

---
+++ Cargo3DSite/Cargo3DSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cargo3DSite.Startup))]
+namespace Cargo3DSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
