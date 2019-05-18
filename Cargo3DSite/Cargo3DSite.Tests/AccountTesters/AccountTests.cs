6c5a934fb998ec46bae34d527fe94f691784a3ad
Jett Tripp <jtripp@student.neumont.edu>
2019-05-18 16:58:22 -0600

    Started making tests for the accounts and permissions

---
+++ Cargo3DSite/Cargo3DSite.Tests/AccountTesters/AccountTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cargo3DSite.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cargo3DSite.Tests.Controllers
+{
+    [TestClass]
+    public class AccountTests
+    {
+
+        private List<ApplicationUser> Users = new List<ApplicationUser>();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var user = new ApplicationUser
+            {
+                UserName = "Jett@Tripp.net",
+                Email = "Jett@Tripp.net"
+            };
+            Users.Add(user);
+
+
+        }
+
+        [TestMethod]
+        public void AddPermisionsToUsers()
+        {
+            Console.WriteLine("Adding permissions to users.");
+        }
+
+    }
+}
