4644c60b44d810f644079b9572c46fc5c48f6aca
Jett Tripp <jtripp@student.neumont.edu>
2019-05-20 17:56:34 -0600

    Mostly added tests for Users and Items
    Still need to connect to a db to validate users and add them. Also need db to create roles and allow for tests to be written (Successfully).
    - 3DItemDisplay model created
    - AccountTester created

--- Cargo3DSite/Cargo3DSite.Tests/AccountTesters/AccountTests.cs
+++ Cargo3DSite/Cargo3DSite.Tests/AccountTesters/AccountTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cargo3DSite.Controllers;
 using Cargo3DSite.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -17,20 +18,84 @@ namespace Cargo3DSite.Tests.Controllers
         [TestInitialize]
         public void Setup()
         {
-            var user = new ApplicationUser
+            var user1 = new ApplicationUser
             {
                 UserName = "Jett@Tripp.net",
                 Email = "Jett@Tripp.net"
             };
-            Users.Add(user);
+            Users.Add(user1);
+
+            var user4 = new ApplicationUser
+            {
+                UserName = "Richard@Sanchez.net",
+                Email = "Richard@Sanchez.net"
+            };
+            Users.Add(user4);
+            
+            var user2 = new ApplicationUser
+            {
+                UserName = "Nick@Hurst.net",
+                Email = "Nick@Hurst.net"
+            };
+            Users.Add(user2);
+
+            var user3 = new ApplicationUser
+            {
+                UserName = "Chris@Ellis.net",
+                Email = "Chris@Ellis.net"
+            };
+            Users.Add(user3);
+
+        }
 
 
+        [TestMethod]
+        public async Task AddUsersWithPasswordsAsync()
+        {
+            //arrange
+
+            AccountController AC = new AccountController();
+            HomeController HC = new HomeController();
+
+
+            String password = "Cargo3d";
+
+            //act
+
+            Console.WriteLine("Adding users.");
+
+            foreach (var user in Users)
+            {
+                var model = new RegisterViewModel()
+                {
+                    Email = user.Email,
+                    Password = password,
+                    ConfirmPassword = password
+                };
+                //await AC.Register(model, true);
+            }
+
+            //assert
+
+            Assert.IsTrue(true, "No errors");
         }
 
         [TestMethod]
         public void AddPermisionsToUsers()
         {
-            Console.WriteLine("Adding permissions to users.");
+            //cant do without db or at least not yet
+        }
+
+        [TestMethod]
+        public void AddItemsToUser()
+        {
+            //arrange
+
+
+
+            //act
+
+            //assert
         }
 
     }
---
+++ Cargo3DSite/Cargo3DSite/Models/3DCreationDisplayModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cargo3DSite.Models
+{
+    public class _3DCreationDisplayModel
+    {
+
+        //image, title, author, description, list of files
+
+        public String ImgUrl { get; set; }
+        public String Title { get; set; }
+        public String AuthorName { get; set; }
+        public String Description { get; set; }
+        public int FileId { get; set; }
+
+    }
+}
