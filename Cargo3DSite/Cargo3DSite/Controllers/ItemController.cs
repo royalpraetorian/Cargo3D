d8b54d4d7ca51383d5ca5514b73bb80a4667f2d8
Richard Sanchez <superpoptart1@gmail.com>
2019-05-29 10:24:59 -0600

    Added Controllers
    Adding the missing controllers myb

---
+++ Cargo3DSite/Cargo3DSite/Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cargo3DSite.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddItem()
+        {
+            ViewBag.Message = "Adding an item.";
+
+            return View();
+        }
+    }
+}
